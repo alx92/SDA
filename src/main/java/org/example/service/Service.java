e005fd7b2260ccbf53b72bbe5b509673f8546832
alx92 <alex.ciobanu92@gmail.com>
2020-11-19 21:27:19 +0200

    space-app SDA final-project

--- src/main/java/org/example/service/Service.java
+++ src/main/java/org/example/service/Service.java
@@ -3,7 +3,7 @@ package org.example.service;
 @org.springframework.stereotype.Service
 public abstract class Service {
 
-    private static final String key = "F2t9eSuecY54NKs8RH7PZ97xdeFXfg6M4A81sVbF";
+    private static final String key = "private key";
 
     public static String getKey() {
         return key;
